3eaea6de43d56d73f7649c0e9aa922591cf6fa88
Ricky Concepcion <rconcep@srn.sandia.gov>
2019-10-03 14:35:36 -0600

    Factored out commercial and residential load profile downloaders

--- es_gui/apps/data_manager/load.py
+++ es_gui/apps/data_manager/load.py
@@ -17,7 +17,7 @@ from kivy.clock import Clock
 from es_gui.apps.data_manager.data_manager import DATA_HOME
 from es_gui.apps.data_manager.utils import check_connection_settings
 from es_gui.resources.widgets.common import InputError, WarningPopup, ConnectionErrorPopup, MyPopup, RecycleViewRow, FADEIN_DUR, LoadingModalView, PALETTE, rgba_to_fraction, fade_in_animation
-from es_gui.downloaders.building_data import get_commercial_geographical_locations, get_building_types, get_building_data
+from es_gui.downloaders.building_data import get_commercial_geographical_locations, get_commercial_building_types, get_building_data, get_residential_load_types, get_residential_geographical_locations
 
 MAX_WHILE_ATTEMPTS = 7
 
@@ -100,7 +100,7 @@ class DataManagerCommercialLoadScreen(Screen):
     def _get_building_types(self, location_root, ssl_verify=True, proxy_settings=None):
         self.connection_error_occurred = False
 
-        building_types, self.connection_error_occurred = get_building_types(
+        building_types, self.connection_error_occurred = get_commercial_building_types(
             location_root,
             ssl_verify=ssl_verify,
             proxy_settings=proxy_settings,
@@ -265,129 +265,39 @@ class DataManagerResidentialLoadScreen(Screen):
             popup.open()
     
     def _get_load_types(self, ssl_verify=True, proxy_settings=None):
-        # Convert root page content to BeautifulSoup.
-        attempt_download = True
-        n_tries = 0
         self.connection_error_occurred = False
 
-        while attempt_download:
-            n_tries += 1
+        load_types, self.connection_error_occurred = get_residential_load_types(
+            ssl_verify=ssl_verify,
+            proxy_settings=proxy_settings,
+            n_attempts=MAX_WHILE_ATTEMPTS
+        )
+        
+        # Populate load types RecycleView.
+        records = [{'name': load_type['name'], 'link': load_type['link']} for load_type in load_types]
+        records = sorted(records, key=lambda t: t['name'])
 
-            if n_tries >= MAX_WHILE_ATTEMPTS:
-                logging.warning('LoadProfileDM: Hit download retry limit.')
-                attempt_download = False
-                self.connection_error_occurred = True
-                break
-            
-            if App.get_running_app().root.stop.is_set():
-                return
-            
-            try:
-                with requests.Session() as req:
-                    page = req.get(RESIDENTIAL_LOAD_ROOT, timeout=10, verify=ssl_verify, proxies=proxy_settings)
-                    if page.status_code != requests.codes.ok:
-                        page.raise_for_status()
-                    else:
-                        attempt_download = False
-            except requests.HTTPError as e:
-                logging.error('LoadProfileDM: {0}'.format(repr(e)))
-            except requests.exceptions.ProxyError:
-                logging.error('LoadProfileDM: Could not connect to proxy.')
-            except requests.ConnectionError as e:
-                logging.error('LoadProfileDM: Failed to establish a connection to the host server.')
-            except requests.Timeout as e:
-                logging.error('LoadProfileDM: The connection timed out.')
-            except requests.RequestException as e:
-                logging.error('LoadProfileDM: {0}'.format(repr(e)))
-            except Exception as e:
-                # Something else went wrong.
-                logging.error('LoadProfileDM: An unexpected error has occurred. ({0})'.format(repr(e)))
-            else: 
-                soup = BeautifulSoup(page.content, 'html.parser')
-                
-                # Get the bulleted list of locations.
-                load_tags = soup.body.ul
-                
-                # Convert bulleted list to DataFrame.
-                load_types = []
-
-                for link in load_tags.find_all('a')[1:]:
-                    # [1:] to skip the link to the parent directory.
-                    type_root = link['href']
-
-                    load_types.append({'name': type_root[:-1], 'link': RESIDENTIAL_LOAD_ROOT + type_root})
-
-                # Populate load types RV.
-                records = [{'name': load_type['name'], 'link': load_type['link']} for load_type in load_types]
-                records = sorted(records, key=lambda t: t['name'])
-
-                self.load_type_rv.data = records
-                self.load_type_rv.unfiltered_data = records
+        self.load_type_rv.data = records
+        self.load_type_rv.unfiltered_data = records
     
     def _get_locations(self, load_root_link, ssl_verify=True, proxy_settings=None):
-        # Convert root page content to BeautifulSoup.
-        attempt_download = True
-        n_tries = 0
         self.connection_error_occurred = False
 
-        while attempt_download:
-            n_tries += 1
+        locations, self.connection_error_occurred = get_residential_geographical_locations(
+            load_root_link,
+            ssl_verify=ssl_verify,
+            proxy_settings=proxy_settings,
+            n_attempts=MAX_WHILE_ATTEMPTS
+        )
 
-            if n_tries >= MAX_WHILE_ATTEMPTS:
-                logging.warning('LoadProfileDM: Hit download retry limit.')
-                attempt_download = False
-                self.connection_error_occurred = True
-                break
-            
-            if App.get_running_app().root.stop.is_set():
-                return
-            
-            try:
-                with requests.Session() as req:
-                    page = req.get(load_root_link, timeout=10, verify=ssl_verify, proxies=proxy_settings)
-                    if page.status_code != requests.codes.ok:
-                        page.raise_for_status()
-                    else:
-                        attempt_download = False
-            except requests.HTTPError as e:
-                logging.error('LoadProfileDM: {0}'.format(repr(e)))
-            except requests.exceptions.ProxyError:
-                logging.error('LoadProfileDM: Could not connect to proxy.')
-            except requests.ConnectionError as e:
-                logging.error('LoadProfileDM: Failed to establish a connection to the host server.')
-            except requests.Timeout as e:
-                logging.error('LoadProfileDM: The connection timed out.')
-            except requests.RequestException as e:
-                logging.error('LoadProfileDM: {0}'.format(repr(e)))
-            except Exception as e:
-                # Something else went wrong.
-                logging.error('LoadProfileDM: An unexpected error has occurred. ({0})'.format(repr(e)))
-            else: 
-                soup = BeautifulSoup(page.content, 'html.parser')
-                
-                # Get the bulleted list of locations.
-                location_tags = soup.body.ul
-                
-                # Convert bulleted list to DataFrame.
-                locations = []
+        self.df_locations = pd.DataFrame.from_records(locations)
 
-                for link in location_tags.find_all('a')[1:]:
-                    # [1:] to skip the link to the parent directory.
-                    loc_root = link['href']
-                    country, state_abbr, name, _, _ = loc_root.split('_')
+        # Populate state RV.
+        records = [{'name': state} for state in self.df_locations['state'].unique()]
+        records = sorted(records, key=lambda t: t['name'])
 
-                    name = ' '.join(name.split('.')[:-1])
-
-                    locations.append({'country': country, 'state': state_abbr, 'name': name, 'link': load_root_link + loc_root})
-
-                self.df_locations = pd.DataFrame.from_records(locations)
-
-                # Populate state RV.
-                records = [{'name': state} for state in self.df_locations['state'].unique()]
-                records = sorted(records, key=lambda t: t['name'])
-
-                self.state_rv.data = records
-                self.state_rv.unfiltered_data = records
+        self.state_rv.data = records
+        self.state_rv.unfiltered_data = records
     
     def on_load_type_selected(self, instance, value):
         try:
--- es_gui/downloaders/building_data.py
+++ es_gui/downloaders/building_data.py
@@ -1,12 +1,10 @@
-"""
-This module contains functions for obtaining load profiles from the OpenEI database.
+"""This module contains functions for obtaining load profiles from the OpenEI database.
 """
 
 from __future__ import absolute_import
 
 import os
 import io
-import threading
 import logging
 
 import requests
@@ -20,10 +18,26 @@ RESIDENTIAL_LOAD_ROOT = DATASET_ROOT + 'RESIDENTIAL_LOAD_DATA_E_PLUS_OUTPUT/'
 
 
 def get_commercial_geographical_locations(ssl_verify=True, proxy_settings=None, n_attempts=7):
-    """
-    Obtains a list of records of available geographical locations for commercial building load profiles.
+    """Returns the available geographical locations for commercial building load profiles.
 
-    Returns: A list of dictionary records 
+    This function checks the root of the commercial building database and documents the available geographical locations that can be selected based on the table of URLs.
+
+    Parameters
+    ----------
+    ssl_verify : bool
+        True if the URL request should use SSL verification, defaults to True
+    proxy_settings : dict
+        HTTP and HTTPS proxies for URL request; format is {'http_proxy': '...', 'https_proxy': '...'}, defaults to None
+    n_attempts : int
+        The maximum number of retries for the URL request before declaring connection errors, defaults to 7
+
+    Returns
+    -------
+    list
+        Records as list of dictionaries with keys ['country', 'name', 'state', 'link']
+    
+    bool
+        True if a connection error occurred during the requests and the number of retries hit the specified limit
     """
     attempt_download = True
     n_tries = 0
@@ -82,14 +96,36 @@ def get_commercial_geographical_locations(ssl_verify=True, proxy_settings=None, 
     return locations, connection_error_occurred
 
 
-def get_building_types(location_root, ssl_verify=True, proxy_settings=None, n_attempts=7):
-    """
+def get_commercial_building_types(location_root, ssl_verify=True, proxy_settings=None, n_attempts=7):
+    """Returns the available commercial building types for a given geographical location.
 
+    This function checks the specified geographical location root URL and documents the available commercial building types that can be selected based on the table of URLs.
+
+    Parameters
+    ----------
+    location_root : str
+        URL for the directory of a geographical location in the commercial building load profile directorate
+    ssl_verify : bool
+        True if the URL request should use SSL verification, defaults to True
+    proxy_settings : dict
+        HTTP and HTTPS proxies for URL request; format is {'http_proxy': '...', 'https_proxy': '...'}, defaults to None
+    n_attempts : int
+        The maximum number of retries for the URL request before declaring connection errors, defaults to 7
+
+    Returns
+    -------
+    list
+        Records as list of dictionaries with keys ['name', 'link']
+    
+    bool
+        True if a connection error occurred during the requests and the number of retries hit the specified limit
     """
     # Convert root page content to BeautifulSoup
     attempt_download = True
     n_tries = 0
     connection_error_occurred = False
+
+    building_types = []
 
     while attempt_download:
         n_tries += 1
@@ -130,8 +166,6 @@ def get_building_types(location_root, ssl_verify=True, proxy_settings=None, n_at
             building_tags = soup.body.ul
             
             # Convert bulleted list to DataFrame
-            building_types = []
-
             for link in building_tags.find_all('a')[1:]:
                 # [1:] to skip the link to the parent directory
                 csv_link = link['href']
@@ -145,8 +179,22 @@ def get_building_types(location_root, ssl_verify=True, proxy_settings=None, n_at
 
 
 def get_building_data(csv_link, save_directory, ssl_verify=True, proxy_settings=None, n_attempts=7):
-    """
+    """Saves the hourly load profile to a .csv file on disk.
 
+    This function saves the load profile at `csv_link` to the directory `save_directory`. The format has two columns: `Date/Time` and `Electricity:Facility [kW](Hourly)`. The filename is based on the specific data obtained. Additional directories will be created in `save_directory` in which the .csv file will be saved.
+
+    Parameters
+    ----------
+    csv_link : str
+        URL for the .csv file containing the load profile
+    save_directory : str
+        Relative path for where load profile will be saved
+    ssl_verify : bool
+        True if the URL request should use SSL verification, defaults to True
+    proxy_settings : dict
+        HTTP and HTTPS proxies for URL request; format is {'http_proxy': '...', 'https_proxy': '...'}, defaults to None
+    n_attempts : int
+        The maximum number of retries for the URL request before declaring connection errors, defaults to 7
     """
     attempt_download = True
     n_tries = 0
@@ -199,6 +247,165 @@ def get_building_data(csv_link, save_directory, ssl_verify=True, proxy_settings=
     return connection_error_occurred
 
 
+def get_residential_load_types(ssl_verify=True, proxy_settings=None, n_attempts=7):
+    """Returns the available load types for residential building load profiles.
+
+    This function checks the database for which types of load profiles are available for residential buildings. Examples include `base`, `high`, and `low`.
+
+    Parameters
+    ----------
+    ssl_verify : bool
+        True if the URL request should use SSL verification, defaults to True
+    proxy_settings : dict
+        HTTP and HTTPS proxies for URL request; format is {'http_proxy': '...', 'https_proxy': '...'}, defaults to None
+    n_attempts : int
+        The maximum number of retries for the URL request before declaring connection errors, defaults to 7
+
+    Returns
+    -------
+    list
+        Records as list of dictionaries with keys ['name', 'link']
+    
+    bool
+        True if a connection error occurred during the requests and the number of retries hit the specified limit
+    """
+    attempt_download = True
+    n_tries = 0
+    connection_error_occurred = False
+
+    load_types = []
+
+    while attempt_download:
+        n_tries += 1
+
+        if n_tries >= n_attempts:
+            logging.warning('LoadProfileDM: Hit download retry limit.')
+            attempt_download = False
+            connection_error_occurred = True
+            break
+        
+        # if App.get_running_app().root.stop.is_set():
+        #     return
+        
+        try:
+            with requests.Session() as req:
+                page = req.get(RESIDENTIAL_LOAD_ROOT, timeout=10, verify=ssl_verify, proxies=proxy_settings)
+                if page.status_code != requests.codes.ok:
+                    page.raise_for_status()
+                else:
+                    attempt_download = False
+        except requests.HTTPError as e:
+            logging.error('LoadProfileDM: {0}'.format(repr(e)))
+        except requests.exceptions.ProxyError:
+            logging.error('LoadProfileDM: Could not connect to proxy.')
+        except requests.ConnectionError as e:
+            logging.error('LoadProfileDM: Failed to establish a connection to the host server.')
+        except requests.Timeout as e:
+            logging.error('LoadProfileDM: The connection timed out.')
+        except requests.RequestException as e:
+            logging.error('LoadProfileDM: {0}'.format(repr(e)))
+        except Exception as e:
+            # Something else went wrong.
+            logging.error('LoadProfileDM: An unexpected error has occurred. ({0})'.format(repr(e)))
+        else: 
+            soup = BeautifulSoup(page.content, 'html.parser')
+            
+            # Get the bulleted list of locations.
+            load_tags = soup.body.ul
+            
+            # Convert bulleted list to DataFrame.
+            for link in load_tags.find_all('a')[1:]:
+                # [1:] to skip the link to the parent directory.
+                type_root = link['href']
+
+                load_types.append({'name': type_root[:-1], 'link': RESIDENTIAL_LOAD_ROOT + type_root})
+    
+    return load_types, connection_error_occurred
+
+
+def get_residential_geographical_locations(load_root_link, ssl_verify=True, proxy_settings=None, n_attempts=7):
+    """Returns the available geographical locations for the specified residential building load type.
+
+    This function checks the database for which geographical locations are available for the specific residential load type at `load_root_link` based on the table of URLs.
+
+    Parameters
+    ----------
+    load_root_link : str
+        URL for the directory of a load type in the residential building load profile directorate
+    ssl_verify : bool
+        True if the URL request should use SSL verification, defaults to True
+    proxy_settings : dict
+        HTTP and HTTPS proxies for URL request; format is {'http_proxy': '...', 'https_proxy': '...'}, defaults to None
+    n_attempts : int
+        The maximum number of retries for the URL request before declaring connection errors, defaults to 7
+
+    Returns
+    -------
+    list
+        Records as list of dictionaries with keys ['country', 'state', 'name', 'link']
+    
+    bool
+        True if a connection error occurred during the requests and the number of retries hit the specified limit
+    """
+    attempt_download = True
+    n_tries = 0
+    connection_error_occurred = False
+
+    locations = []
+
+    while attempt_download:
+        n_tries += 1
+
+        if n_tries >= n_attempts:
+            logging.warning('LoadProfileDM: Hit download retry limit.')
+            attempt_download = False
+            connection_error_occurred = True
+            break
+        
+        # if App.get_running_app().root.stop.is_set():
+        #     return
+        
+        try:
+            with requests.Session() as req:
+                page = req.get(load_root_link, timeout=10, verify=ssl_verify, proxies=proxy_settings)
+                if page.status_code != requests.codes.ok:
+                    page.raise_for_status()
+                else:
+                    attempt_download = False
+        except requests.HTTPError as e:
+            logging.error('LoadProfileDM: {0}'.format(repr(e)))
+        except requests.exceptions.ProxyError:
+            logging.error('LoadProfileDM: Could not connect to proxy.')
+        except requests.ConnectionError as e:
+            logging.error('LoadProfileDM: Failed to establish a connection to the host server.')
+        except requests.Timeout as e:
+            logging.error('LoadProfileDM: The connection timed out.')
+        except requests.RequestException as e:
+            logging.error('LoadProfileDM: {0}'.format(repr(e)))
+        except Exception as e:
+            # Something else went wrong.
+            logging.error('LoadProfileDM: An unexpected error has occurred. ({0})'.format(repr(e)))
+        else: 
+            soup = BeautifulSoup(page.content, 'html.parser')
+            
+            # Get the bulleted list of locations.
+            location_tags = soup.body.ul
+            
+            # Convert bulleted list to DataFrame.
+            locations = []
+
+            for link in location_tags.find_all('a')[1:]:
+                # [1:] to skip the link to the parent directory.
+                loc_root = link['href']
+                country, state_abbr, name, _, _ = loc_root.split('_')
+
+                name = ' '.join(name.split('.')[:-1])
+
+                locations.append({'country': country, 'state': state_abbr, 'name': name, 'link': load_root_link + loc_root})
+    
+    return locations, connection_error_occurred
+
+
 if __name__ == '__main__':
     ssl_verify = False
     proxy_settings = {
@@ -208,12 +415,13 @@ if __name__ == '__main__':
 
     download_all = False
 
-    locations, connection_error_occured = get_commercial_geographical_locations(ssl_verify, proxy_settings)
+    # Commercial
+    locations, connection_error_occurred = get_commercial_geographical_locations(ssl_verify, proxy_settings)
 
     if download_all:
         for location in locations:
             location_root = location['link']
-            building_types, connection_error_occured = get_building_types(location_root, ssl_verify, proxy_settings)
+            building_types, connection_error_occurred = get_commercial_building_types(location_root, ssl_verify, proxy_settings)
 
             for building_type in building_types:    
                 csv_link = building_type['link']
@@ -225,9 +433,37 @@ if __name__ == '__main__':
                 get_building_data(csv_link, save_directory=destination_dir, ssl_verify=False, proxy_settings=proxy_settings)
     else:
         location_root = locations[-1]['link']
-        building_types, connection_error_occured = get_building_types(location_root, ssl_verify, proxy_settings)
+        building_types, connection_error_occurred = get_commercial_building_types(location_root, ssl_verify, proxy_settings)
  
         csv_link = building_types[-1]['link']
+
+        url_split = csv_link.split('/')
+        destination_dir = os.path.join('', url_split[-2])
+        os.makedirs(destination_dir, exist_ok=True)
+
+        get_building_data(csv_link, save_directory=destination_dir, ssl_verify=False, proxy_settings=proxy_settings)
+    
+    # Residential
+    load_types, connection_error_occurred = get_residential_load_types(ssl_verify, proxy_settings)
+
+    if download_all:
+        for load_type in load_types:
+            load_type_root = load_type['link']
+            locations, connection_error_occurred = get_residential_geographical_locations(load_type_root, ssl_verify, proxy_settings)
+
+            for location in locations:    
+                csv_link = location['link']
+
+                url_split = csv_link.split('/')
+                destination_dir = os.path.join('building_data', url_split[-2])
+                os.makedirs(destination_dir, exist_ok=True)
+
+                get_building_data(csv_link, save_directory=destination_dir, ssl_verify=False, proxy_settings=proxy_settings)
+    else:
+        load_type_root = load_types[-1]['link']
+        locations, connection_error_occurred = get_residential_geographical_locations(load_type_root, ssl_verify, proxy_settings)
+
+        csv_link = locations[-1]['link']
 
         url_split = csv_link.split('/')
         destination_dir = os.path.join('', url_split[-2])
