865efdfd31c6c4dad41fa71bbc5dfc4c89661543
Ricky Concepcion <rconcep@srn.sandia.gov>
2019-12-03 12:29:14 -0700

    Added indicator to HelpCarousel for which slide in the deck you are in

--- es_gui/proving_grounds/help_carousel.py
+++ es_gui/proving_grounds/help_carousel.py
@@ -3,9 +3,10 @@ import os
 
 import pandas as pd
 
+from kivy.uix.checkbox import CheckBox
 from kivy.uix.modalview import ModalView
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty, BooleanProperty
+from kivy.properties import StringProperty, BooleanProperty, NumericProperty
 
 from es_gui.resources.widgets.common import RecycleViewRow, WarningPopup
 
@@ -16,35 +17,56 @@ class HelpCarouselSlide(BoxLayout):
 
 class HelpCarouselModalView(ModalView):
     """A ModalView with a series of prompts for importing time series data from a csv file.
-    
-    Parameters
-    ----------
-    write_directory : str
-        Path of directory where the imported data will be written
-
-    write_function : func
-        Function describing how to write the imported data to a persistent file
-    
-    chooser_description : str
-        Description displayed on the DataImporter file chooser screen
-    
-    format_description : str
-        Description displayed on the DataImporter format analyzer screen
-    
-    data_validation_function : str
-        Function used to validate the selected data
-
-    Notes
-    -----
-    ``write_function`` should handle saving the persistent object (e.g., csv file) to disk.
-    ``data_validation_function`` should raise a ValueError to indicate failing validation with a relevant reason why it failed.    
     """
+    current_slide_index = NumericProperty()
+
     def add_slides(self, slide_deck):
+        """Adds image and text to a new slide in the carousel slide deck.
+
+        Each slide consists of a large image on the left and accompanying text on the right.
+
+        Parameters
+        ----------
+        slide_deck : list(tuple)
+            Content for each slide (source, caption) where the source is the path to the slide image and the caption is the text
+        
+        Notes
+        -----
+        The source is relative to the current working directory (alongside main.py).
         """
-        """
+        self.slide_progress_radio_buttons = []
+
         for source, caption in slide_deck:
             slide = HelpCarouselSlide()
             slide.img.source = source
             slide.img_caption.text = caption
 
             self.carousel.add_widget(slide)
+            self.slide_progress_radio_buttons.append(SlideProgressRadioButton())
+            
+        for ix, button in enumerate(self.slide_progress_radio_buttons):
+            button.active = ix == 0
+            self.slide_progress_bx.add_widget(button)
+    
+    def change_slide(self, direction):
+        """Changes carousel slide in the specified direction.
+        """
+        getattr(self.carousel, 'load_{0}'.format(direction))()
+
+        if direction == 'previous':
+            destination_slide = self.carousel.previous_slide
+        else:
+            destination_slide = self.carousel.next_slide
+        
+        if destination_slide is not None:
+            self.current_slide_index = self.carousel.slides.index(destination_slide)
+    
+    def on_current_slide_index(self, instance, value):
+        # Changes the active button in the slide progress group to reflect the new slide.
+        self.slide_progress_radio_buttons[value].active = True
+
+
+class SlideProgressRadioButton(CheckBox):
+    """Radio button representing progress within the HelpCarousel slide deck.
+    """
+    pass
