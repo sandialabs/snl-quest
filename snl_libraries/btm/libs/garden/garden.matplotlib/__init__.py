9164e703e5bbeeff85727cc4606097daf55094d8
Tu Nguyen <tunguy@sandia.gov>
2024-03-26 17:15:42 -0600

    QuESt 2.0 Update

---
+++ snl_libraries/btm/libs/garden/garden.matplotlib/__init__.py
@@ -0,0 +1,10 @@
+from kivy.garden.matplotlib.backend_kivy import FigureCanvasKivy,\
+                                FigureManagerKivy, RendererKivy,\
+                                GraphicsContextKivy, NavigationToolbar2Kivy,\
+                                MPLKivyApp
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+
+__all__ = (FigureCanvasKivy.__name__, FigureManagerKivy.__name__,
+           RendererKivy.__name__, GraphicsContextKivy.__name__,
+           NavigationToolbar2Kivy.__name__, MPLKivyApp.__name__,
+           FigureCanvasKivyAgg.__name__)
