4703e9bb587f5bd42fdb4082fb36d4ad4755b500
Nguyen <tunguy@sandia.gov>
2026-04-22 15:14:23 -0600

    Update workspace subflow case handling and pandas pin

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         "kivy==2.3.0",
         "numpy==1.26.4",
-        "pandas==2.2.1",
+        "pandas>=2.2.3,<2.3",
         "pyomo==6.7.1",
         "matplotlib==3.7.4",
         "six==1.16.0",
